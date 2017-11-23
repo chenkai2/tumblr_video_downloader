0a9a826fab6d1c3613ec04cd3ceccf5cd42b00fb
chenkai2 <orochi114@163.com>
2017-11-24 01:42:21 +0800

    expand download area to 30*30 download control instead of download icon

--- src/tumblr.js
+++ src/tumblr.js
@@ -117,7 +117,7 @@ var Tumblr = {
 		$downloadControl.title = "Download";
 		var $downloadIcon = document.createElement('div');
 		$downloadIcon.className = "icon_download";
-		$downloadIcon.onclick = function(ev) {
+		$downloadControl.onclick = function(ev) {
 			//$downloadIcon.className = "icon_dotdotdot";
 			chrome.runtime.sendMessage({
 					"action": "addDownloadQueue",
