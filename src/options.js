578c2785ffc8d4feff9e23e38c6648b6789cfdc1
chenkai2 <orochi114@163.com>
2017-11-28 23:37:02 +0800

    adjust locale formatter

--- src/options.js
+++ src/options.js
@@ -4,45 +4,43 @@
  * @date    2017-11-28 02:35:05
  * @version $Id$
  */
-
+//i18n
+document.body.innerHTML = document.body.innerHTML
+    .replace(/{__MSG_(.+)__}/g, function($match, $p1, $offset, $string) {
+        //console.log($match, $p1);
+        return chrome.i18n.getMessage($p1);
+    });
 var $setting = document.getElementById('setting'),
-	$saveAs = document.getElementById('save_as'),
-	$globalVolume = document.getElementById('global_volume'),
-	i18n = chrome.i18n.getMessage;
+    $saveAs = document.getElementById('save_as'),
+    $globalVolume = document.getElementById('global_volume');
 $saveAs.addEventListener('click', function() {
-	$saveAs.classList.toggle('active');
-	var $saveAsFlag = $saveAs.classList.contains('active');
-	options.set({
-		'saveAs': $saveAsFlag
-	});
+    $saveAs.classList.toggle('active');
+    var $saveAsFlag = $saveAs.classList.contains('active');
+    options.set({
+        'saveAs': $saveAsFlag
+    });
 });
 $globalVolume.addEventListener('click', function() {
-	$globalVolume.classList.toggle('active');
-	var $globalVolumeFlag = $globalVolume.classList.contains('active');
-	options.set({
-		'globalVolume': $globalVolumeFlag
-	});
+    $globalVolume.classList.toggle('active');
+    var $globalVolumeFlag = $globalVolume.classList.contains('active');
+    options.set({
+        'globalVolume': $globalVolumeFlag
+    });
 });
-//i18n
-$setting.innerHTML = i18n('options_setting');
-$saveAs.title = i18n('options_save_as_title');
-$saveAs.getElementsByTagName('span')[0].innerText = i18n('options_save_as_label');
-$globalVolume.title = i18n('options_global_volume_title');
-$globalVolume.getElementsByTagName('span')[0].innerText = i18n('options_global_volume_label');
 //init setting
 options.get({
-	'saveAs': true
+    'saveAs': true
 }, function($i) {
-	var $saveAsFlag = $i.saveAs;
-	if ($saveAsFlag) {
-		$saveAs.classList.add('active');
-	}
+    var $saveAsFlag = $i.saveAs;
+    if ($saveAsFlag) {
+        $saveAs.classList.add('active');
+    }
 });
 options.get({
-	'globalVolume': true
+    'globalVolume': true
 }, function($i) {
-	var $globalVolumeFlag = $i.globalVolume;
-	if ($globalVolumeFlag) {
-		$globalVolume.classList.add('active');
-	}
+    var $globalVolumeFlag = $i.globalVolume;
+    if ($globalVolumeFlag) {
+        $globalVolume.classList.add('active');
+    }
 });
